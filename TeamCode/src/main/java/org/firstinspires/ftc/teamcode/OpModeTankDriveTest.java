0ae704fef794337682961bd8ee4002f72d911531
Rovio01 <roviononincorporated@gmail.com>
2016-10-18 09:40:12 -0400

    Created two simple test opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTankDriveTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by Benjamin Jensrud on 10/18/2016.
+ * Allows manual control of a tank drive system
+ */
+@TeleOp(name = "Tank Drive Test" , group = "Tests")
+public class OpModeTankDriveTest extends LinearOpMode {
+
+    DcMotor left = null;
+    DcMotor right = null;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        left = hardwareMap.dcMotor.get("left");
+        right = hardwareMap.dcMotor.get("right");
+        left.setDirection(DcMotor.Direction.FORWARD);
+        right.setDirection(DcMotor.Direction.REVERSE);
+        waitForStart();
+        runtime.reset();
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            left.setPower(0.2*Math.cbrt(gamepad1.left_stick_y));
+            right.setPower(0.2*Math.cbrt(gamepad1.right_stick_y));
+        }
+    }
+}
