01350b76815c7aeacb50433a2f2742030e3c5168
Rovio01 <roviononincorporated@gmail.com>
2017-01-20 16:59:41 -0500

    Updated for the second competition robot configuration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeFullManual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeFullManual.java
@@ -20,29 +20,29 @@ public class OpModeFullManual extends OpMode {
     //r-right
     //f-front
     //b-back
-    DcMotor lfd, lbd, rfd, rbd, conveyor, leftLauncher, rightLauncher;
-    Servo leftPushServo, rightPushServo, frontServo;
-    ColorSensor leftColor, rightColor, bottomColor;
+    DcMotor lfd, lbd, rfd, rbd, conveyor, leftLauncher, rightLauncher, drawerSlide;
+    Servo frontServo, capBallServo;
+    //ColorSensor leftColor, rightColor, bottomColor;
 
     @Override
     public void init() {
-        lfd=hardwareMap.dcMotor.get("lfd");
-        lbd=hardwareMap.dcMotor.get("lbd");
-        rfd=hardwareMap.dcMotor.get("rfd");
-        rbd=hardwareMap.dcMotor.get("rbd");
+        lfd           = hardwareMap.dcMotor.get("lfd");
+        lbd           = hardwareMap.dcMotor.get("lbd");
+        rfd           = hardwareMap.dcMotor.get("rfd");
+        rbd           = hardwareMap.dcMotor.get("rbd");
 
-        conveyor=hardwareMap.dcMotor.get("conveyor");
-        leftPushServo=hardwareMap.servo.get("leftpushservo");
-        rightPushServo=hardwareMap.servo.get("rightpushservo");
-        frontServo=hardwareMap.servo.get("frontservo");
-        leftLauncher=hardwareMap.dcMotor.get("leftlauncher");
-        rightLauncher=hardwareMap.dcMotor.get("rightlauncher");
+        conveyor      = hardwareMap.dcMotor.get("conveyor");
+        frontServo    = hardwareMap.servo  .get("frontservo");
+        leftLauncher  = hardwareMap.dcMotor.get("leftlauncher");
+        rightLauncher = hardwareMap.dcMotor.get("rightlauncher");
+		drawerSlide   = hardwareMap.dcMotor.get("drawerSlide");
+		capBallServo  = hardwareMap.servo  .get("capBallServo");
 
-        //leftColor=hardwareMap.colorSensor.get("leftcolor");
-        rightColor=hardwareMap.colorSensor.get("rightcolor");
-        //bottomColor=hardwareMap.colorSensor.get("bottomcolor");
+        //leftColor   = hardwareMap.colorSensor.get("leftcolor");
+        //rightColor  = hardwareMap.colorSensor.get("rightcolor");
+        //bottomColor = hardwareMap.colorSensor.get("bottomcolor");
 
-        rightColor.enableLed(false);
+        //rightColor.enableLed(false);
 
         lfd.setDirection(DcMotor.Direction.REVERSE);
         lbd.setDirection(DcMotor.Direction.REVERSE);
@@ -52,36 +52,18 @@ public class OpModeFullManual extends OpMode {
         leftLauncher.setDirection(DcMotorSimple.Direction.REVERSE);
 
         frontServo.setPosition(0.5);
-
-        leftPushServo.setPosition(0.482);
-        rightPushServo.setPosition(0.3658);
     }
 
     @Override
     public void loop() {
-        telemetry.addData("Red: ", ""+rightColor.red());
-        telemetry.addData("Blue: ", ""+rightColor.blue());
-        telemetry.update();
+        //telemetry.addData("Red: ", ""+rightColor.red());
+        //telemetry.addData("Blue: ", ""+rightColor.blue());
+        //telemetry.update();
 
         lfd.setPower((1-0.5*Math.abs(gamepad1.right_stick_y))*gamepad1.left_stick_x+0.5*gamepad1.right_stick_x);
         lbd.setPower((1-0.5*Math.abs(gamepad1.right_stick_y))*gamepad1.left_stick_y+0.5*gamepad1.right_stick_x);
         rfd.setPower((1-0.5*Math.abs(gamepad1.right_stick_y))*gamepad1.left_stick_y-0.5*gamepad1.right_stick_x);
         rbd.setPower((1-0.5*Math.abs(gamepad1.right_stick_y))*gamepad1.left_stick_x-0.5*gamepad1.right_stick_x);
-
-        if (gamepad1.right_bumper) {
-            leftPushServo.setPosition(0);
-        }
-        else {
-            leftPushServo.setPosition(0.482);
-        }
-
-        if (gamepad1.left_bumper) {
-            rightPushServo.setPosition(0.88318);
-        }
-        else {
-            rightPushServo.setPosition(0.3658);
-        }
-
 
         if (gamepad2.right_bumper) {
             rightLauncher.setPower(0.2);
@@ -92,6 +74,22 @@ public class OpModeFullManual extends OpMode {
             leftLauncher.setPower(0);
         }
 
+        if (gamepad1.right_bumper) {
+            drawerSlide.setPower(1);
+        }
+		else if (gamepad1.left_bumper) {
+			drawerSlide.setPower(-1);
+		}
+		else {
+			drawerSlide.setPower(0);
+		}
+
+		if (gamepad1.a) {
+			capBallServo.setPosition(-1.0);
+		}
+		else {
+			capBallServo.setPosition(1.0);
+		}
 
         frontServo.setPosition(0.5*(gamepad2.left_stick_y+1));
 
